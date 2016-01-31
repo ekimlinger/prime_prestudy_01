b121a2b0492035e675c3de80b840a1d265f47a12
ekimlinger <ekimlinger@gmail.com>
2016-01-31 14:10:40 -0600

    Bonus challenge functions added to js

--- gitChallenge.js
+++ gitChallenge.js
@@ -1,1 +1,13 @@
-console.log("Hey this works!\n\nI'm in the console!")
+console.log("Hey this works!\n\nI'm in the console!\n\nNow I am going to perform a function");
+
+function foo (int){
+	int *=2;
+	console.log("Your number multiplied by 2: " + int + "\n");
+	return int;
+}
+
+var bar = 1;
+
+console.log("The starting number is: " + bar + "\n");
+
+foo(foo(foo(foo(foo(foo(foo(foo(bar))))))));
